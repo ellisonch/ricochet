7a70f5657486630847878b70343ac4afa8a1922f
chucky <cme@freefour.com>
2014-02-15 01:00:20 -0500

    tons of improvements to test client

--- Ricochet/TestClient/TestClient.cs
+++ Ricochet/TestClient/TestClient.cs
@@ -14,13 +14,20 @@ namespace TestClient {
     // serialization based on interface
     // consider auto registering public methods/etc
     // x/y
+    // TODO need to start using volatile in places
     class TestClient {
-        private static IEnumerable<bool> IterateUntilFalse(Func<bool> condition) {
-            while (condition()) yield return true;
-        }
+        // play with these
+        const bool reportServer = false;
+        const bool reportClient = true;
         const int reportServerStatsTimer = 5000;
         const int reportClientStatsTimer = 5000;
         const int numThreads = 16;
+        const string payloadPrefix = smallPayload; // or bigPayload
+
+        // probably don't play with these
+        const int numDistinctPayloads = 10;
+        const int numWarmupQueries = 5;
+
 
         static ConcurrentBag<long> times = new ConcurrentBag<long>();
         static ConcurrentDictionary<int, double> timeSums = new ConcurrentDictionary<int, double>();
@@ -28,28 +35,43 @@ namespace TestClient {
         static long failures = 0;
         static long count = 0;
 
+        static ManualResetEvent threadsReady = new ManualResetEvent(false);
+        static volatile int numThreadsReady = 0;
+
         static Client client;
+
+        static ConcurrentDictionary<int, Thread> threads = new ConcurrentDictionary<int, Thread>();
+        static string[] payloads = new string[numDistinctPayloads];
 
         static int Main(string[] args) {
             TestClient tc = new TestClient();
 
-            string junk = smallPayload;
-            junk = bigPayload;
-
             client = new Client("127.0.0.1", 11000, WhichSerializer.Serializer);
             client.WaitUntilConnected();
 
-            // new Thread(ReportServerStats).Start(client);
-            new Thread(ReportClientStats).Start(client);
+            if (reportServer) {
+#pragma warning disable 0162
+                new Thread(ReportServerStats).Start(client);
+#pragma warning restore 0162
+            }
+            if (reportClient) {
+#pragma warning disable 0162
+                ReportClientStatsHeader();
+                new Thread(ReportClientStats).Start(client);
+#pragma warning restore 0162
+            }
 
-            List<Thread> threads = new List<Thread>();
+            for (int i = 0; i < numDistinctPayloads; i++) {
+                payloads[i] = payloadPrefix + i;
+            }
+
             for (int i = 0; i < numThreads; i++) {
                 timeSums[i] = 0.0;
                 Thread t = new Thread(ClientWorker);
-                threads.Add(t);
+                threads[i] = t;
                 t.Start(i);
             }
-            foreach (var thread in threads) {
+            foreach (var thread in threads.Values) {
                 thread.Join();
             }
             return 0;
@@ -57,63 +79,123 @@ namespace TestClient {
 
         private static void ClientWorker(object obj) {
             int myThreadNum = (int)obj;
+            numThreadsReady++;
+
+            // this waits until all the threads have been created and are here
+            while (threadsReady.WaitOne(1)) {
+                if (numThreadsReady == numThreads) {
+                    threadsReady.Set();
+                }
+            }
+            warmup();
+
             while (true) {
                 var mycount = Interlocked.Increment(ref count);
-                var payload = smallPayload + mycount;
-                //var payload = bigPayload + mycount;
-
-                var q = new AQuery(payload);
-                long myfailures;
 
                 Stopwatch mysw = Stopwatch.StartNew();
-                AResponse ar = null;
-                bool success;
-                try {
-                    success = client.TryCall<AQuery, AResponse>("double", q, out ar);
-                } catch (ObjectDisposedException) {
-                    success = false;
-                }
+                bool success = doCall(mycount, payloads[mycount % numDistinctPayloads]);
                 mysw.Stop();
 
-                if (!success) {
-                    myfailures = Interlocked.Increment(ref failures);
-                } else {
+                long myfailures;
+                if (success) {
                     times.Add(mysw.ElapsedTicks);
                     timeSums[myThreadNum] += mysw.ElapsedMilliseconds;
-                    myfailures = Interlocked.Read(ref failures);
-                    Debug.Assert(ar.res == payload + payload, String.Format("Something went wrong, {0} != {1}", ar.res, payload + payload));
+                } else {
+                    myfailures = Interlocked.Increment(ref failures);
                 }
             }
         }
 
+        private static void warmup() {
+            var q = new AQuery("xxx");
+            AResponse ar = null;
+            for (int i = 0; i < numWarmupQueries; i++) {
+                client.TryCall<AQuery, AResponse>("double", q, out ar);
+            }
+        }
+
+        private static bool doCall(long myCount, string payload) {
+            var q = new AQuery(payload);
+            bool success = false;
+
+            try {
+                AResponse ar = null;
+                if (client.TryCall<AQuery, AResponse>("double", q, out ar)) {
+                    // Debug.Assert(ar.res == payload + payload, String.Format("Something went wrong, {0} != {1}", ar.res, payload + payload));
+                    Debug.Assert(ar.res == payload + payload, "Something went wrong, strings didn't match");
+                    success = true;
+                }
+            } catch (Exception e) {
+                Console.WriteLine("Something really unexpected happened: {0}", e);
+                success = false;
+            }
+            return success;
+        }
+
+        private static void ReportClientStatsHeader() {
+            Console.WriteLine("\nThe game is to maximize throughput (responses per second (rps)),\nwhile keeping individual response times low.\n");
+
+            Console.WriteLine("\nThe output first shows stuff about instantaneous numbers (across the most recent {0} seconds).", reportClientStatsTimer/1000.0);
+            Console.Write("Avg (inst) response time (ms)");
+            Console.Write(", (Max");
+            Console.Write(", Stddev");
+            Console.Write(", 99 Pctile");
+            Console.Write(", 99.99 Pctile)");
+            Console.Write(", Requests per second");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Next come overall numbers from the duration of the program.");
+            Console.Write("Min");
+            Console.Write(", Avg");
+            Console.Write(", Max");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Finally are general stats.");
+            Console.Write("Total requests processed, total failures");
+            Console.WriteLine("\n");
+
+        }
         static double ticksPerMS = (Stopwatch.Frequency / 1000.0);
+        static List<double> arls = new List<double>();
         private static void ReportClientStats(object obj) {
             Client client = (Client)obj;
             while (client.IsAlive) {
-                var myCount = Interlocked.Read(ref count);
-                var myFailures = Interlocked.Read(ref failures);
-                var myTimes = times;
-                times = new ConcurrentBag<long>();
-                if (myTimes.Count > 0) {
-                    double avg = myTimes.Average() / ticksPerMS;
-                    double max = myTimes.Max() / ticksPerMS;
-                    double numer = myTimes.Sum(time => ((time / ticksPerMS) - avg) * ((time / ticksPerMS) - avg));
-                    double stddev = Math.Sqrt(numer / myTimes.Count());
-
-                    Console.WriteLine("{0:0.000} arl ({1:#,#00.} mrl, {2:0.000} stdrl); {3:#,###.} rps (done: {4}, failures: {5})",
-                        avg,
-                        max,
-                        stddev,
-                        myTimes.Count / (reportClientStatsTimer / 1000.0),
-                        myCount,
-                        myFailures
-                    );
-
-                    //foreach (var time in myTimes) {
-                    //    Console.WriteLine(time / ticksPerMS);
-                    //}
-                }
                 System.Threading.Thread.Sleep(reportClientStatsTimer);
+
+                var myTimes = times.ToArray();
+                if (myTimes.Length == 0) {
+                    continue;
+                }
+                var theTotal = Interlocked.Read(ref count);
+                
+                Array.Sort(myTimes);
+                var percentile99 = myTimes[(int)(myTimes.Length * 0.99)] / ticksPerMS;
+                var percentile9999 = myTimes[(int)(myTimes.Length * 0.9999)] / ticksPerMS;
+                var myCount = myTimes.Count();
+                var myFailures = Interlocked.Read(ref failures);
+                
+                times = new ConcurrentBag<long>();
+                double avg = myTimes.Average() / ticksPerMS;
+                double max = myTimes.Max() / ticksPerMS;
+                double numer = myTimes.Sum(time => ((time / ticksPerMS) - avg) * ((time / ticksPerMS) - avg));
+                double stddev = Math.Sqrt(numer / myCount);
+
+                arls.Add(avg);
+
+                Console.Write("{0:0.000} ({1:#,000.}, {2:0.00}, {3:00.00}, {4:00.00}), {5:#,###.} rps",
+                    avg,
+                    max,
+                    stddev,
+                    percentile99,
+                    percentile9999,
+                    myTimes.Length / (reportClientStatsTimer / 1000.0)
+                );
+                Console.Write(" | {0:0.000} {1:0.000} {2:0.000}", arls.Min(), arls.Average(), arls.Max());
+                Console.Write(" | done: {0:#,###.}; fail: {1}",
+                    theTotal,
+                    myFailures
+                );
+                Console.WriteLine();
             }
         }
 
@@ -143,7 +225,7 @@ namespace TestClient {
                 Console.WriteLine("----------------------------------------------");
             }
         }
-        private static string smallPayload = "foo bar baz";
-        private static string bigPayload = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus odio sollicitudin porta condimentum. Maecenas non rutrum sapien, dictum tincidunt nibh. Donec lacinia mattis interdum. Quisque pellentesque, ligula non elementum vulputate, massa lacus mattis justo, at iaculis mi lorem vel neque. Aenean cursus vitae nulla non vehicula. Vestibulum venenatis urna ac turpis semper, sed molestie nibh convallis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra sodales ante dapibus malesuada. Morbi in lectus vulputate tortor elementum congue id quis sem. Duis eget commodo magna. Suspendisse luctus viverra pharetra. Nam lacinia eros id dictum posuere. Ut euismod, enim sit amet laoreet dictum, enim erat adipiscing eros, nec auctor nibh elit sit amet turpis. Morbi hendrerit nibh a urna congue, ac ultricies tellus vulputate. Integer ac velit venenatis, porttitor tellus eu, pretium sapien. Curabitur eget tincidunt odio, ut vehicula nisi. Praesent molestie diam nullam.";
+        const string smallPayload = "foo bar baz";
+        const string bigPayload = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus odio sollicitudin porta condimentum. Maecenas non rutrum sapien, dictum tincidunt nibh. Donec lacinia mattis interdum. Quisque pellentesque, ligula non elementum vulputate, massa lacus mattis justo, at iaculis mi lorem vel neque. Aenean cursus vitae nulla non vehicula. Vestibulum venenatis urna ac turpis semper, sed molestie nibh convallis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra sodales ante dapibus malesuada. Morbi in lectus vulputate tortor elementum congue id quis sem. Duis eget commodo magna. Suspendisse luctus viverra pharetra. Nam lacinia eros id dictum posuere. Ut euismod, enim sit amet laoreet dictum, enim erat adipiscing eros, nec auctor nibh elit sit amet turpis. Morbi hendrerit nibh a urna congue, ac ultricies tellus vulputate. Integer ac velit venenatis, porttitor tellus eu, pretium sapien. Curabitur eget tincidunt odio, ut vehicula nisi. Praesent molestie diam nullam.";
     }
 }
