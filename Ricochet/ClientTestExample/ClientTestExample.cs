04065329102b0545aad0d29d20b88905a69117ee
chucky <chucky@okcupid.com>
2014-04-25 09:39:26 -0400

    simplified creating payloads in test

--- Ricochet/ClientTestExample/ClientTestExample.cs
+++ Ricochet/ClientTestExample/ClientTestExample.cs
@@ -14,16 +14,20 @@ namespace ClientTestExample {
             tc.Start();
         }
 
-        public ClientTestExample(string[] args) : base(args, "double", new MessagePackWithCustomMessageSerializer()) { }
+        const int distinctPayloads = 50;
+
+        private List<AQuery> payloads = new List<AQuery>(distinctPayloads);
+
+        public ClientTestExample(string[] args) : base(args, "double", new MessagePackWithCustomMessageSerializer()) {
+            for (int i = 0; i < distinctPayloads; i++) {
+                const string payloadPrefix = "foo bar baz";
+                string payload = payloadPrefix + i;
+                payloads.Add(new AQuery(payload));
+            }
+        }        
 
         protected override AQuery QueryGen(long num) {
-            // string payload = payloads[mycount % numDistinctPayloads];
-
-            const string payloadPrefix = "foo bar baz";
-            //const string payloadPrefix = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus odio sollicitudin porta condimentum. Maecenas non rutrum sapien, dictum tincidunt nibh. Donec lacinia mattis interdum. Quisque pellentesque, ligula non elementum vulputate, massa lacus mattis justo, at iaculis mi lorem vel neque. Aenean cursus vitae nulla non vehicula. Vestibulum venenatis urna ac turpis semper, sed molestie nibh convallis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra sodales ante dapibus malesuada. Morbi in lectus vulputate tortor elementum congue id quis sem. Duis eget commodo magna. Suspendisse luctus viverra pharetra. Nam lacinia eros id dictum posuere. Ut euismod, enim sit amet laoreet dictum, enim erat adipiscing eros, nec auctor nibh elit sit amet turpis. Morbi hendrerit nibh a urna congue, ac ultricies tellus vulputate. Integer ac velit venenatis, porttitor tellus eu, pretium sapien. Curabitur eget tincidunt odio, ut vehicula nisi. Praesent molestie diam nullam.";
-
-            string payload = payloadPrefix + num;
-            return new AQuery(payload);
+            return payloads[(int)(num % (long)distinctPayloads)];
         }
     }
 }
